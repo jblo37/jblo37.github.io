f638b48b231c80febf19bfb141678eddd2938ab4
Juan Pablo Santamaria Hernandez <139196249+jblo37@users.noreply.github.com>
2024-08-07 23:29:07 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const API_KEY = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjFjNDcxNTg4LTZkMWQtNGQ1YS1iMmFjLTZjNDM5MzY5NmQxNCIsImlhdCI6MTcyMzA5MTY1MSwic3ViIjoiZGV2ZWxvcGVyL2YwOTM0YTg4LTRkYzMtNWUyOC1lMDFkLWRiZjIyZjdkMzY1NCIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5nIn0seyJjaWRycyI6WyI0NS43OS4yMTguNzkiXSwidHlwZSI6ImNsaWVudCJ9XX0.GmalFLWjbERMCJ4PKzcvZuaXHQoCAZxgfklUzexrmfd6t1zywazTFRZhRxCplawnzAUVDySt9-b7CR0Mnjt0eQ';
 const PLAYER_TAG = '9QGGLVUV2';
 const PROXY_URL = 'https://proxy.royaleapi.dev/v1/players/%23';
-const API_URL = `${PROXY_URL}${PLAYER_TAG}`;
+const API_URL = `${PROXY_URL}${PLAYER_TAG}/warlog`;
 
 fetch(API_URL, {
   method: 'GET',
@@ -11,9 +11,11 @@ fetch(API_URL, {
 })
 .then(response => response.json())
 .then(data => {
-  const participants = data.memberList;
-  participants.forEach(member => {
-    console.log(`Miembro: ${member.name}, Puntos de Carrera del Río: ${member.riverRacePoints}`);
+  data.items.forEach(war => {
+    console.log(`Fecha de la guerra: ${war.createdDate}`);
+    war.participants.forEach(participant => {
+      console.log(`Miembro: ${participant.name}, Puntos de Carrera del Río: ${participant.riverRacePoints}`);
+    });
   });
 })
 .catch(error => console.error('Error:', error));
