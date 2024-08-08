08e0661a90253337fe291180d9c6147172274eb7
Juan Pablo Santamaria Hernandez <139196249+jblo37@users.noreply.github.com>
2024-08-07 22:18:04 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const API_KEY = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6ImY4MjBkZjJhLTZmZGEtNDJiNS05NmU0LTMyNmJiZGVlNDg3YyIsImlhdCI6MTcyMjIwNTE0Mywic3ViIjoiZGV2ZWxvcGVyL2I5MjBmNmIyLTI0MDAtZGM3Zi1lZGM2LWYxNzI4MTYzMDYyOCIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5nIn0seyJjaWRycyI6WyIxODkuMTQzLjIyNS4yMzgiXSwidHlwZSI6ImNsaWVudCJ9XX0.HBuxPnSC6XhL3eJ4UuUqRImRRA1PfjKGz5UPiAqm9pn800-u_k0K7AicrxBjCDffpwIHKLIczydyG6fV-3gROg';
-const API_URL = 'https://api.clashroyale.com/v1/players/%239QGGLVUV2';
+const PLAYER_TAG = '9QGGLVUV2';
+const PROXY_URL = 'https://cors-anywhere.herokuapp.com/';
+const API_URL = `https://api.clashroyale.com/v1/players/%23${PLAYER_TAG}`;
 
-fetch(API_URL, {
+fetch(PROXY_URL + API_URL, {
   method: 'GET',
   headers: {
     'Authorization': `Bearer ${API_KEY}`
