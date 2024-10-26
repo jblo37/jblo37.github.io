862fcbf4a982cc93da56e2ff324d9f938714d01b
Juan Pablo Santamaria Hernandez <139196249+jblo37@users.noreply.github.com>
2024-10-25 19:59:50 -0600

    Add files via upload

---
+++ testeo/assets/js/insignia.js
@@ -0,0 +1,3 @@
+const insignia = document.getElementById('insignia');
+const actInsignia = '<a class="navbar-brand" href="../index.html">Guerreros Mayas<img class="ms-2"src="https://cdn.statsroyale.com/v5/clan_leagues/full/legend_league_2_16000028.webp" width="auto" height="35" alt="insignia" loading=""></a>';
+insignia.innerHTML = actInsignia;
