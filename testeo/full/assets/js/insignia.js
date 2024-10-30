66c404000489ccfd32e2e40c758f95ed1e195e2b
Juan Pablo Santamaria Hernandez <139196249+jblo37@users.noreply.github.com>
2024-10-29 19:03:05 -0600

    Update insignia.js

--- testeo/full/assets/js/insignia.js
+++ testeo/full/assets/js/insignia.js
@@ -2,5 +2,5 @@ const insignia = document.getElementById('insignia');
 const actInsignia = 
 
 
-'<a class="navbar-brand" href="../index.html">Guerreros Mayas<img class="ms-2"src="https://cdn.statsroyale.com/v5/clan_leagues/full/legend_league_1_16000028.webp" width="auto" height="35" alt="insignia" loading=""></a>';
+'<a class="navbar-brand" href="">Guerreros Mayas<img class="ms-2"src="https://cdn.statsroyale.com/v5/clan_leagues/full/legend_league_1_16000028.webp" width="auto" height="35" alt="insignia" loading=""></a>';
 insignia.innerHTML = actInsignia;
